8dedd401c83fc529f5e5fbf7fdeb38ec9b1e0a3d
TommiAL <tommi.lipponen@student.careeria.fi>
2019-06-09 13:45:54 +0300

    Luotiin TuotteetController ja TilausDBModel

---
+++ TilausXIITommiAL/TilausXIITommiAL/Controllers/TuotteetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TilausXIITommiAL.Controllers
+{
+    public class TuotteetController : Controller
+    {
+        // GET: Tuotteet
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
